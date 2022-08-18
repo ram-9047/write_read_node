cfa4c75ae7b17d747c9e580dfd54baa80ea23c36
ram-9047 <ramg786@live.com>
2022-08-18 16:52:14 +0530

    adds nodemon package

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require("http");
 const routes = require("./routes");
 const port = 8080;
 // const server = http.createServer(routes);
+
 const server = http.createServer(routes.handler);
 
 server.listen(port, () => {
